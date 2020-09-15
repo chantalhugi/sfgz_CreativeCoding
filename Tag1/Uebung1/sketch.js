ed2431a646b1d378291de85f890b4d6b5c182cd3
Chantal Hugentobler <chantal_hugi@hotmail.com>
2020-09-15 19:52:29 +0200

    Uebung 1

--- Tag1/Uebung1/sketch.js
+++ Tag1/Uebung1/sketch.js
@@ -1,16 +1,63 @@
+var circleX = 0;
+
+function draw() {
+circleX = circleX + 1;
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(287, 201, 222);
-  fill('#000088	');
-  stroke(90, 94, 200);
-  strokeWeight(90);
-  ellipse(width/2, height/2,200,200);
+  background(0,104,139);
+  noFill();
 
-  stroke(95, 211, 80);
+  stroke(233,150,122);
+
+
+  fill(141,238,238);
+  strokeWeight(10);
+  ellipse(300,300,590,590);
+  ellipse(300,300,590,590);
+
+  noFill();
   strokeWeight(4);
-  fill('#008888');
- rect(50,180,300,40);
+  ellipse(300,300,75,75);
+
+  strokeWeight(6);
+  ellipse(300,300,204,204);
+
+
+
+  strokeWeight(8);
+  ellipse(300,300,424,424);
+
+
+
+  strokeWeight(8);
+  stroke(139,28,98);
+  line(0, 0, 600, 600);
+  line(300, 0, 0, 300);
+  line(300, 0, 600, 300);
+  line(0, 300, 300, 600);
+  line(300, 0, 600, 300);
+  line(300, 600, 600, 300);
+  line(600,  0, 0, 600);
+  line(300, 0, 300, 600);
+  line(0, 300, 600, 300);
+
+  noFill();
+  strokeWeight(4);
+  square(273, 273, 56);
+
+  strokeWeight(6);
+  square(225, 225, 150);
+
+  strokeWeight(8);
+  square(150, 150, 300);
+
+  strokeWeight(10);
+  square(5, 5, 590);
+
+
 }
