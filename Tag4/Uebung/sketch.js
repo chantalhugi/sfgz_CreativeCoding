48a1a93ef0ee92b9819609efe1a2ec489b639c36
Chantal Hugentobler <chantal_hugi@hotmail.com>
2020-10-03 16:11:45 +0200

    new

--- Tag4/Uebung/sketch.js
+++ Tag4/Uebung/sketch.js
@@ -1,52 +1,44 @@
-var gui;
+//var gui;
 
-var anz = 5;
-var distanz = 25;
+//var anz = 5;
+//var distanz = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
-  gui = createGui('p5.gui');
-  gui.addGlobals('anz', 'distanz');
-  // only call draw when then gui is changed
-  noLoop();
+//  gui = createGui('p5.gui');
+//  gui.addGlobals('anz', 'distanz');
+//  noLoop();
+stroke(255, 255, 255);
+strokeWeight(0.1);
+ background(10, 79, 119);
+
 }
 
+
+let x = [0, 100, 200, 300, 400, 700, 1200];
+let y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+let shiftY=0;
+
+
+
 function draw() {
-
-    let h = map(mouseX, 0, width, 0, 360);
-    let b = 1;
-
-  background(h, 79, 119);
-  strokeWeight(1);
-//  translate(0, -200)
-
-  for (let i = 2; i <= anz; i++) {
-
-
-  //  fill(b, 108, 166, 200);
-    stroke('#CCFFFF');
+    translate(0,shiftY);
     beginShape();
-    vertex(-10, 900);
-    bezierVertex(1000 + distanz * i, 200 - distanz * i, 1000 + distanz * i, 100 - distanz * i, 3000 + distanz * i, 1200 - distanz * i);
-    endShape(CLOSE)
+    //curveVertex(0, y[0]);
+        for (let i = 0; i < x.length; i++) {
 
-  //  fill(b, 230, 230, 150);
-    stroke('#CCFFFF');
-    beginShape();
-    vertex(10, 900);
-    bezierVertex(-500 + distanz * i, 200 - distanz * i, 0 + distanz * i, 0 - distanz * i, 2000 + distanz * i, 2000 - distanz * i);
-    endShape(CLOSE)
+            curveVertex(x[i], y[i]);
+            //vertex(x[i], y[i]);
+            x[i] += random(-1,1); //neu initialisierung
+            y[i] += random(-1,1); //neu initialisierung
+        }
+    //curveVertex(width, y[y.length-1]);
+    endShape();
+    shiftY+=0.2;
 
-
-  //  fill(b, 054, 100, 100);
-    stroke('rgb(176,196,222)');
-    beginShape();
-    vertex(10, 900);
-    bezierVertex(-500 + distanz * i, 800 - distanz * i, 500 + distanz * i, 10 - distanz * i, 2000 + distanz * i, 1000 - distanz * i);
-    endShape(CLOSE)
-
-
-
-  }
+    if(shiftY > height){
+      shiftY=0;
+    }
 }
