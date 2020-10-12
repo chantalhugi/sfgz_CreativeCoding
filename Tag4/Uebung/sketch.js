48482ca483929cf7389cc8ff1a0fe3779c7f9052
Chantal Hugentobler <chantal_hugi@hotmail.com>
2020-10-12 19:52:27 +0200

    neue einstellungen + gui Regler dazu

--- Tag4/Uebung/sketch.js
+++ Tag4/Uebung/sketch.js
@@ -1,23 +1,23 @@
-//var gui;
+var gui;
 
-//var anz = 5;
-//var distanz = 25;
+var anz = 5;
+var distanz = 25;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
-//  gui = createGui('p5.gui');
-//  gui.addGlobals('anz', 'distanz');
-//  noLoop();
-stroke(255, 255, 255);
-strokeWeight(0.1);
- background(10, 79, 119);
+  gui = createGui('p5.gui');
+  gui.addGlobals('anz', 'distanz');
+//noLoop();
+  stroke(255, 255, 255);
+  strokeWeight(0.5);
+  background(10, 79, 119);
 
 }
 
 
-let x = [0, 100, 200, 300, 400, 700, 1200];
-let y = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let x = [0, 100, 200, 300, 400, 700, 2200];
+let y = [, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 let shiftY=0;
 
@@ -26,17 +26,17 @@ let shiftY=0;
 function draw() {
     translate(0,shiftY);
     beginShape();
-    //curveVertex(0, y[0]);
+    curveVertex(x, y[0]);
         for (let i = 0; i < x.length; i++) {
 
             curveVertex(x[i], y[i]);
             //vertex(x[i], y[i]);
-            x[i] += random(-1,1); //neu initialisierung
-            y[i] += random(-1,1); //neu initialisierung
+            x[i] += random(-1 + distanz * i,1 - distanz * i);
+            y[i] += random(-1 + distanz * i,1 - distanz * i);
         }
-    //curveVertex(width, y[y.length-1]);
+    curveVertex(width, y[y.length-1]);
     endShape();
-    shiftY+=0.2;
+    shiftY+=0.5;
 
     if(shiftY > height){
       shiftY=0;
