c957df9a4222b9bca88a6b43608e3305dd2c96e4
Chantal Hugentobler <chantal_hugi@hotmail.com>
2020-10-17 09:43:33 +0200

    neu

--- Tag3/Uebung4.2/sketch.js
+++ Tag3/Uebung4.2/sketch.js
@@ -17,6 +17,9 @@ function draw() {
     let h = map(mouseX, 0, width, 0, 360);
     let b = 1;
 
+    randomSeed(9);
+
+
   background(h, 79, 119);
   strokeWeight(1);
 //  translate(0, -200)
@@ -25,18 +28,19 @@ function draw() {
 
 
   //  fill(b, 108, 166, 200);
+
     stroke('#CCFFFF');
     beginShape();
     vertex(-10, 900);
     bezierVertex(1000 + distanz * i, 200 - distanz * i, 1000 + distanz * i, 100 - distanz * i, 3000 + distanz * i, 1200 - distanz * i);
-    endShape(CLOSE)
+    endShape()
 
   //  fill(b, 230, 230, 150);
     stroke('#CCFFFF');
     beginShape();
     vertex(10, 900);
     bezierVertex(-500 + distanz * i, 200 - distanz * i, 0 + distanz * i, 0 - distanz * i, 2000 + distanz * i, 2000 - distanz * i);
-    endShape(CLOSE)
+    endShape()
 
 
   //  fill(b, 054, 100, 100);
@@ -44,9 +48,7 @@ function draw() {
     beginShape();
     vertex(10, 900);
     bezierVertex(-500 + distanz * i, 800 - distanz * i, 500 + distanz * i, 10 - distanz * i, 2000 + distanz * i, 1000 - distanz * i);
-    endShape(CLOSE)
-
-
+    endShape()
 
   }
 }
--- Tag4/Uebung/sketch.js
+++ Tag4/Uebung/sketch.js
@@ -8,37 +8,59 @@ function setup() {
   noFill();
   gui = createGui('p5.gui');
   gui.addGlobals('anz', 'distanz');
-//noLoop();
-  stroke(255, 255, 255);
-  strokeWeight(0.5);
-  background(10, 79, 119);
-
+  // only call draw when then gui is changed
+  noLoop();
 }
 
-
-let x = [0, 100, 200, 300, 400, 700, 2200];
-let y = [, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-let shiftY=20;
-
-
-
 function draw() {
-    translate(0,shiftY);
+
+    let h = map(mouseX, 0, width, 0, 360);
+    let b = 1;
+
+    randomSeed(9);
+
+
+  background(h, 79, 119);
+  strokeWeight(1);
+//  translate(0, -200)
+
+  for (let i = 2; i <= anz; i++) {
+
+
+  //  fill(b, 108, 166, 200);
+
+    stroke('#CCFFFF');
+    /*
     beginShape();
-    curveVertex(x, y[0]);
-        for (let i = 0; i < x.length; i++) {
+    vertex(-10, 900);
+    bezierVertex(1000 + distanz * i, 200 - distanz * i, 1000 + distanz * i, 100 - distanz * i, 3000 + distanz * i, 1200 - distanz * i);
+    endShape()
 
-            curveVertex(x[i], y[i]);
-            //vertex(x[i], y[i]);
-            x[i] += random(-1 + distanz * i,1 - distanz * i);
-            y[i] += random(-1 + distanz * i,1 - distanz * i);
-        }
-    curveVertex(width, y[y.length-1]);
-    endShape();
-    shiftY+=0.5;
+  //  fill(b, 230, 230, 150);
+    stroke('#CCFFFF');
+    beginShape();
+    vertex(10, 900);
+    bezierVertex(-500 + distanz * i, 200 - distanz * i, 0 + distanz * i, 0 - distanz * i, 2000 + distanz * i, 2000 - distanz * i);
+    endShape()
 
-    if(shiftY > height){
-      shiftY=10;
-    }
+
+  //  fill(b, 054, 100, 100);
+    stroke('rgb(176,196,222)');
+    beginShape();
+    vertex(10, 900);
+    bezierVertex(-500 + distanz * i, 800 - distanz * i, 500 + distanz * i, 10 - distanz * i, 2000 + distanz * i, 1000 - distanz * i);
+    endShape()
+*/
+
+
+beginShape();
+curveVertex(30, 100 +random (- 1 * distanz/5, distanz/5) + distanz * i);
+curveVertex(80, 20 +random (- 1 * distanz/5, distanz/5) + distanz * i);
+curveVertex(50, 100 +random (- 1 * distanz/5, distanz/5) + distanz * i);
+curveVertex(100, 80 +random (- 1 * distanz/5, distanz/5) + distanz * i);
+curveVertex(150, 100 +random (- 1 * distanz/5, distanz/5) + distanz * i);
+curveVertex(200, 10 +random (- 1 * distanz/5, distanz/5) + distanz * i);
+endShape();
+
+  }
 }
