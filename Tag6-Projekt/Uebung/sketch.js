54f9d4b57f54ad8760da20e53994e583dfe3f434
Chantal Hugentobler <chantal_hugi@hotmail.com>
2020-10-29 18:07:16 +0100

    letzte anpassungen

--- Tag6-Projekt/Uebung/sketch.js
+++ Tag6-Projekt/Uebung/sketch.js
@@ -20,19 +20,18 @@ function setup() {
 
   gui = createGui('p5.gui');
   gui.addGlobals('wind');
-  // only call draw when then gui is changed
-  // noLoop();
+
 
   input = createInput();
   input.position(20, height - 50);
   button = createButton('SUBMIT');
   button.position(input.x + input.width + 10, height - 50);
-  button.mousePressed(reloadJson); //wenn der Button gedrueckt wird, müssen neue Daten von der API geladen werden (neuer Ort), dies macht die Funktion reloadJson
+  button.mousePressed(reloadJson); 
 
 }
 
 function draw() {
-  background(0, 128, 128);
+  background(0, 44, 107);
   randomSeed(9);
 
   if (up == 0) {
@@ -63,8 +62,8 @@ function draw() {
 
 function welle(i) {
   push()
-  translate(0, +600);
-  stroke(255, 255, 255, 1);
+  translate(0, +650);
+  stroke(190, 220, 250, 1);
   strokeWeight(1);
   beginShape();
   curveVertex(0 + random(-1 * distanz / 4, distanz / 2) + distanz * i, 0 / teiler + random(+1 * distanz / 4, distanz / 2) - distanz * i);
@@ -81,15 +80,15 @@ function welle(i) {
 function welle3(i) {
   push()
   translate(0, +700);
-  stroke(255, 255, 255, 1);
-  strokeWeight(1);
+  stroke(190, 220, 250, 1);
+  strokeWeight(2);
   beginShape();
   curveVertex(0 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 0 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
-  curveVertex(-800 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 1000 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
-  curveVertex(200 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 250 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
-  curveVertex(400 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 500 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
-  curveVertex(600 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 220 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
-  curveVertex(900 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 520 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
+  curveVertex(-800 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 100 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
+  curveVertex(0 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 250 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
+  curveVertex(200 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 500 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
+  curveVertex(550 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 220 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
+  curveVertex(850 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 520 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
   curveVertex(1050 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 200 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
   curveVertex(1200 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 590 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
   curveVertex(1450 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 180 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
@@ -102,21 +101,21 @@ function welle3(i) {
 
 function welle2(i) {
   push()
-  translate(0, +550);
-  stroke(255, 255, 255, 1);
-  strokeWeight(2);
+  translate(0, +600);
+  stroke(190, 220, 250, 1);
+  strokeWeight(1.5);
   beginShape();
-  curveVertex(0 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 0 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(-500 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 150 / teiler + random(-1 * distanz / 2, distanz / 5) - distanz * i - 10);
-  curveVertex(100 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 500 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(350 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 600 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(650 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 350 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(850 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 620 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(1100 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 380 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(1300 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 550 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(1500 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 450 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(1750 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 500 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
-  curveVertex(2000 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 500 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
+  curveVertex(0 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 0 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(-600 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 350 / teiler + random(-1 * distanz / 5, distanz / 5) - distanz * i - 10);
+  curveVertex(100 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 500 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(350 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 600 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(650 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 350 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(850 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 620 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(1100 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 380 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(1300 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 550 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(1500 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 450 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(1750 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 500 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
+  curveVertex(2000 + random(-1 * distanz / 5, distanz / 2) + distanz * i - 10, 500 / teiler + random(-1 * distanz / 5, distanz / 2) - distanz * i - 10);
   endShape();
   pop()
 
@@ -125,9 +124,9 @@ function welle2(i) {
 
 function welle4(i) {
   push()
-  translate(0, +800);
-  stroke(255, 255, 255, 1);
-  strokeWeight(0.7);
+  translate(0, +850);
+  stroke(190, 220, 250, 1);
+  strokeWeight(1);
   beginShape();
   curveVertex(0 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 0 / teiler + random(-1 * distanz / 2, distanz / 2) - distanz * i - 10);
   curveVertex(-700 + random(-1 * distanz / 2, distanz / 2) + distanz * i - 10, 300 / teiler + random(-1 * distanz / 2, distanz / 5) - distanz * i - 10);
@@ -148,9 +147,9 @@ function welle4(i) {
 
 function welle5(i) {
   push()
-  translate(0, +870);
-  stroke(255, 255, 255, 1);
-  strokeWeight(2.5);
+  translate(0, +880);
+  stroke(190, 220, 250, 1);
+  strokeWeight(2.8);
   beginShape();
   curveVertex(0 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 0 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
   curveVertex(-1300 + random(-1 * distanz / 3, distanz / 3) + distanz * i, 1000 / teiler * 2 + random(-1 * distanz / 3, distanz / 3) - distanz * i);
@@ -170,20 +169,15 @@ function welle5(i) {
 
 }
 
-function gotWeather(weather) {
-  // Get the wind speed in km
-  wind = weather.current.wind_speed; // Angaben in km!
-  //  windstaerke = map(windstaerke, 0, 200, 0, 10); // in Rotationsgrad mappen. Bei 200stdkm dreht sich das Windrad nun 10 Grad weiter pro Frame
 
+function gotWeather(weather) {
+  wind = weather.current.wind_speed; // Angaben in km!
   console.log(wind)
 
 }
 
 function reloadJson() {
   let ort = input.value();
-  let url = 'http://api.weatherstack.com/current?access_key=' + key + '&query=' + ort;
+  let url = 'https://api.weatherstack.com/current?access_key=' + key + '&query=' + ort;
   loadJSON(url, gotWeather);
 }
-
-
-// bei teiler Windstarke einfügen von API
